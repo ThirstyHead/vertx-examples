4979207a35d6fb4cbc2bc3bf3044071d03fff892
Scott Davis <scott@thirstyhead.com>
2016-04-23 19:10:26 -0600

    wrapped functionality in exports.vertxStart

--- event/consumer.js
+++ event/consumer.js
@@ -1,27 +1,28 @@
-var eb = vertx.eventBus();
+exports.vertxStart = function(){
+  var eb = vertx.eventBus();
 
-var consumer = eb.consumer('news-feed');
+  var consumer = eb.consumer('news-feed');
 
-// called once per message
-consumer.handler(function(message){
-    console.log('Received news: ' + message.body() );
-});
+  // called once per message
+  consumer.handler(function(message){
+      console.log('Received news: ' + message.body() );
+  });
 
-// called once upon successful registration
-consumer.completionHandler(function (res, res_err) {
-  if (res_err == null) {
-    console.log("The 'news-feed' handler registration has reached all nodes");
-  } else {
-    console.log("Registration failed!");
-  }
-});
+  // called once upon successful registration
+  consumer.completionHandler(function (res, res_err) {
+    if (res_err == null) {
+      console.log("The 'news-feed' handler registration has reached all nodes");
+    } else {
+      console.log("Registration failed!");
+    }
+  });
 
-// called once upon successful UNregistration
-consumer.unregister(function (res, res_err) {
-  if (res_err == null) {
-    console.log("The 'news-feed' handler un-registration has reached all nodes");
-  } else {
-    console.log("Un-registration failed!");
-  }
-});
-
+  // called once upon successful UNregistration
+  consumer.unregister(function (res, res_err) {
+    if (res_err == null) {
+      console.log("The 'news-feed' handler un-registration has reached all nodes");
+    } else {
+      console.log("Un-registration failed!");
+    }
+  });  
+}
--- event/publisher.js
+++ event/publisher.js
@@ -1,10 +1,10 @@
-var eb = vertx.eventBus();
+exports.vertxStart = function(){
+  var eb = vertx.eventBus();
 
-vertx.setPeriodic(1000, function (id) {
-  // This handler will get called every second
-  console.log('Publishing at: ' + new Date() );
-  eb.publish("news-feed", "Greetings from publisher.js");
-});
+  vertx.setPeriodic(1000, function (id) {
+    // This handler will get called every second
+    console.log('Publishing at: ' + new Date() );
+    eb.publish("news-feed", "Greetings from publisher.js");
+  });
 
-
-
+}
